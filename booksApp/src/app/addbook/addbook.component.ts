340c5e602782317682f724f3bbc0d09b23be0738
Alston Dias <alston.dias1423@gmail.com>
2021-11-15 20:04:02 +0530

    changes to admin panel

--- booksApp/src/app/addbook/addbook.component.ts
+++ booksApp/src/app/addbook/addbook.component.ts
@@ -10,11 +10,14 @@ import { BooksdataService } from '../booksdata.service';
   styleUrls: ['./addbook.component.css']
 })
 export class AddbookComponent implements OnInit {
-  bookList!: Array<Book>;
+  bookList : Array<Book> =[];
   id !: number
   title!: string
   author !: string
   category: string = ""
+  categories: Array<string>=[]
+
+  uniquecat : Array<string>=[]
   // addbookFrom !: FormGroup
   constructor(private booksdataService: BooksdataService, private router: Router) {
     // this.addbookFrom = new FormGroup({
@@ -29,32 +32,42 @@ export class AddbookComponent implements OnInit {
     this.booksdataService.getBooks().subscribe(
       (books) => {
         this.bookList = books;
+        for(var b of this.bookList){
+          console.log(b)
+          this.categories.push(b.category)
+        }
+        this.uniquecat = [...new Set(this.categories)]
+
+        console.log("after uniquecat "+this.uniquecat)
       },
       (err) => console.log('Error in fetching data')
     );
-    
+    // for (var i of this.categories){
+    //   console.log("indexes"+i)
+    // }
+    // console.log()
   }
   addNewBook(form: { value: {id:any, title: any; author: any; category: any; }; }) {
-    //set form values
     const newBookData={ 
       _id:form.value.id,
       title:form.value.title,
       author:form.value.author,
       category:form.value.category,
       isRented:false,
+      username:""
     };
 console.log(newBookData);
-// newBookData passed to addBook()
     this.booksdataService.addBook(newBookData).subscribe(data=>{
       console.log(data);
     })
-    this.toInventory()
+    
+    this.router.navigate(['inventory'])
     alert("book added successfully")
   }
 
-  
   toInventory() {
     this.router.navigate(['inventory']);
   }
 
 }
+// { _id: this.id, title: this.title, author: this.author, category: this.category, isRented: false }
